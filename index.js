3f4485afa4af25368ac64b42f1e9dc0a12d96825
GaryGosh <clichethe1@yahoo.com>
2020-07-28 04:04:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://JohnDoe:JohnDoe123@youtube-clone.sutkg.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+{useNewUrlParser: true}).then(() => console.log('DB connected'))
+                        .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+app.listen(5000);
