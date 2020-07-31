6f8d521e4bbf85eae7d5f17a2e56527682930068
GaryGosh <clichethe1@yahoo.com>
2020-08-01 02:11:07 +0530

    heroku fix

--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.json({'Hello': 'I am happy to deploy my application'})
+})
+
 app.get('/api/user/auth', auth, (req, res) => {
     res.status(200).json({
         _id: req.id,
